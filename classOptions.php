93de98ae890ab7637f8a61e2a1cda39ec2217651
jrenouf <renoufjoseph@gmail.com>
2014-10-16 10:58:32 +0200

    push

--- classOptions.php
+++ classOptions.php
@@ -43,7 +43,9 @@ Usage: ./master.php [options]
 options:
 	-h ; --help				Show help message
 	-n			<rounds>	Defines the max number of turns to crack the code
+							Default is 10
 	-l			<rounds>	Defines the maximum number of rounds AI has to decrypt code
+							Default is 5040
 	-a			[=games]	Run auto mode AI vs AI, games defines number of games. 
 							Default is 10
 	All options values must be INT
